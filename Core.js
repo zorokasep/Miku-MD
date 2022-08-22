8912ef3f0bff6323b5876b9986f74a77bd7252f6
Fantox01 <105273285+Fantox01@users.noreply.github.com>
2022-08-22 23:08:50 +0530

    Update Core.js

--- Core.js
+++ Core.js
@@ -2527,18 +2527,19 @@ let mentioned = participants.map(v => v.jid)
 
 
 
-    case 'nowa': {
+    case 'nowa':  case 'stalk': case 'stalknumber':{
         if (isBan) return reply(mess.banned)
-        if (!args[0]) return reply(`Kirim perintah ${prefix+command} <nomer>`)
-        var noteks = args[0]
-        if (!noteks.includes('x')) return reply('lah?')
-        reply(mess.waiting)
+        if (!args[0]) return reply(`Use command like: ${prefix}stalk <number>xxx`)
+        var inputnumber = args[0]
+        if (!inputnumber.includes('x')) return reply('You didnot added x')
+        reply(`Searching for WhatsApp account in given range...`)
+        reply(`Please wait while i fetch details...`)
         function countInstances(string, word) {
         return string.split(word).length - 1;
         }
-        var nomer0 = noteks.split('x')[0]
-        var nomer1 = noteks.split('x')[countInstances(noteks, 'x')] ? noteks.split('x')[countInstances(noteks, 'x')] : ''
-        var random_length = countInstances(noteks, 'x')
+        var number0 = inputnumber.split('x')[0]
+        var number1 = inputnumber.split('x')[countInstances(inputnumber, 'x')] ? inputnumber.split('x')[countInstances(inputnumber, 'x')] : ''
+        var random_length = countInstances(inputnumber, 'x')
         var randomxx;
         if (random_length == 1) {
             randomxx = 10
@@ -2547,26 +2548,26 @@ let mentioned = participants.map(v => v.jid)
         } else if (random_length == 3) {
             randomxx = 1000
         }
-        var nomerny = `LIST NOMER WHATSAPP\n\nPunya Bio/status/info\n`
-        var no_bio = `\nTanpa Bio/status/info || \nHey there! I am using WhatsApp.\n`
-        var no_watsap = `\nTidak Terdaftar\n`
+        var nomerny = `*『 List of Whatsapp Numbers 』*\n\n`
+        var nobio = `\n*Bio:* || \nHey there! I am using WhatsApp.\n`
+        var nowhatsapp = `\n*Numbers with no WhatsApp account within the range you provided*\n`
         for (let i = 0; i < randomxx; i++) {
         var nu = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-        var dom1 = nu[Math.floor(Math.random() * nu.length)]
-        var dom2 = nu[Math.floor(Math.random() * nu.length)]
-        var dom3 = nu[Math.floor(Math.random() * nu.length)]
+        var status1 = nu[Math.floor(Math.random() * nu.length)]
+        var status2 = nu[Math.floor(Math.random() * nu.length)]
+        var status3 = nu[Math.floor(Math.random() * nu.length)]
         var dom4 = nu[Math.floor(Math.random() * nu.length)]
         var rndm;
         if (random_length == 1) {
-        rndm = `${dom1}`
+        rndm = `${status1}`
         } else if (random_length == 2) {
-        rndm = `${dom1}${dom2}`
+        rndm = `${status1}${status2}`
         } else if (random_length == 3) {
-        rndm = `${dom1}${dom2}${dom3}`
+        rndm = `${status1}${status2}${status3}`
         } else if (random_length == 4) {
-        rndm = `${dom1}${dom2}${dom3}${dom4}`
+        rndm = `${status1}${status2}${status3}${dom4}`
         }
-        var anu = await Miku.onWhatsApp(`${nomer0}${i}${nomer1}@s.whatsapp.net`);
+        var anu = await Miku.onWhatsApp(`${number0}${i}${number1}@s.whatsapp.net`);
         var anuu = anu.length !== 0 ? anu : false
         try {
         try {
@@ -2575,15 +2576,15 @@ let mentioned = participants.map(v => v.jid)
         var anu1 = '401'
         }
         if (anu1 == '401' || anu1.status.length == 0) {
-        no_bio += `wa.me/${anu[0].jid.split("@")[0]}\n`
+        nobio += `wa.me/${anu[0].jid.split("@")[0]}\n`
         } else {
-        nomerny += `wa.me/${anu[0].jid.split("@")[0]}\nBiography : ${anu1.status}\nDate : ${moment(anu1.setAt).tz('Asia/Jakarta').format('HH:mm:ss DD/MM/YYYY')}\n\n`
+        nomerny += `🎀 *Number:* wa.me/${anu[0].jid.split("@")[0]}\n🔹 *Bio :* ${anu1.status}\n🔸 *Updated On :* ${moment(anu1.setAt).tz('Asia/Kolkata').format('HH:mm:ss DD/MM/YYYY')}\n\n`
         }
         } catch {
-        no_watsap += `${nomer0}${i}${nomer1}\n`
+        nowhatsapp += `${number0}${i}${number1}\n`
         }
         }
-        reply(`${nomerny}${no_bio}${no_watsap}`)
+        reply(`${nomerny}${nobio}${nowhatsapp}`)
         }
         break
 
@@ -4870,7 +4871,7 @@ Here's the list of my Commands.
 
  *━━━━━━〈  🎆 Core 🎆  〉━━━━━━*
 
-profile, help, delete, deleteall, listgc, listpc, welcome, support, repo, script 
+stalk, profile, help, delete, deleteall, listgc, listpc, welcome, support, repo, script 
  
  *━━━━━━〈  🎀 Owner 🎀  〉━━━━━━*
 
