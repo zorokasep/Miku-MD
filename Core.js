0358289423b2c3eb29c7d30cf485d35e3f9badec
Fantox01 <105273285+Fantox01@users.noreply.github.com>
2022-08-22 22:25:53 +0530

    Update Core.js

--- Core.js
+++ Core.js
@@ -2509,7 +2509,7 @@ if (isBanChat) return reply(mess.bangc)
 
 */
 
-case 'purge':{
+case 'purge':{mess
     if (isBan) return reply(mess.banned)	 			
      if (isBanChat) return reply(mess.bangc)
      if (!m.isGroup) return replay(mess.grouponly)
@@ -2517,19 +2517,79 @@ case 'purge':{
      if (!isAdmins && !isCreator) return replay(mess.useradmin)
 const delay = time => new Promise(res=>setTimeout(res,time));
 let mentioned = participants.map(v => v.jid)
-
-//let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
- //    await Miku.groupParticipantsUpdate(m.chat, [users], 'remove')
-//let using = mentioned.filter(u => !(u == isCreator || u.includes(Miku.user.jid)))
-      for (let member of mentioned) {
-      //if (member.endsWith('@s.whatsapp.net')) 
-      //await delay(3000)
+      for (let member of mentioned) {     
       Miku.groupParticipantsUpdate(m.chat, [member], 'remove')
       }
     }
 
-    
     break
+
+
+
+
+    case 'nowa': {
+        if (isBan) return reply(mess.banned)
+        if (!args[0]) return reply(`Kirim perintah ${prefix+command} <nomer>`)
+        var noteks = args[0]
+        if (!noteks.includes('x')) return reply('lah?')
+        reply(mess.waiting)
+        function countInstances(string, word) {
+        return string.split(word).length - 1;
+        }
+        var nomer0 = noteks.split('x')[0]
+        var nomer1 = noteks.split('x')[countInstances(noteks, 'x')] ? noteks.split('x')[countInstances(noteks, 'x')] : ''
+        var random_length = countInstances(noteks, 'x')
+        var random;
+        if (random_length == 1) {
+        random = 10
+        } else if (random_length == 2) {
+        random = 100
+        } else if (random_length == 3) {
+        random = 1000
+        }
+        var nomerny = `LIST NOMER WHATSAPP\n\nPunya Bio/status/info\n`
+        var no_bio = `\nTanpa Bio/status/info || \nHey there! I am using WhatsApp.\n`
+        var no_watsap = `\nTidak Terdaftar\n`
+        for (let i = 0; i < random; i++) {
+        var nu = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+        var dom1 = nu[Math.floor(Math.random() * nu.length)]
+        var dom2 = nu[Math.floor(Math.random() * nu.length)]
+        var dom3 = nu[Math.floor(Math.random() * nu.length)]
+        var dom4 = nu[Math.floor(Math.random() * nu.length)]
+        var rndm;
+        if (random_length == 1) {
+        rndm = `${dom1}`
+        } else if (random_length == 2) {
+        rndm = `${dom1}${dom2}`
+        } else if (random_length == 3) {
+        rndm = `${dom1}${dom2}${dom3}`
+        } else if (random_length == 4) {
+        rndm = `${dom1}${dom2}${dom3}${dom4}`
+        }
+        var anu = await Miku.onWhatsApp(`${nomer0}${i}${nomer1}@s.whatsapp.net`);
+        var anuu = anu.length !== 0 ? anu : false
+        try {
+        try {
+        var anu1 = await Miku.fetchStatus(anu[0].jid)
+        } catch {
+        var anu1 = '401'
+        }
+        if (anu1 == '401' || anu1.status.length == 0) {
+        no_bio += `wa.me/${anu[0].jid.split("@")[0]}\n`
+        } else {
+        nomerny += `wa.me/${anu[0].jid.split("@")[0]}\nBiography : ${anu1.status}\nDate : ${moment(anu1.setAt).tz('Asia/Jakarta').format('HH:mm:ss DD/MM/YYYY')}\n\n`
+        }
+        } catch {
+        no_watsap += `${nomer0}${i}${nomer1}\n`
+        }
+        }
+        reply(`${nomerny}${no_bio}${no_watsap}`)
+        }
+        break
+
+
+
+
 
 
 
