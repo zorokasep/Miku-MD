4fc150ebeb7ad3091f4dceec577d85774a3be3d0
kirito14122 <112892302+kirito14122@users.noreply.github.com>
2022-11-11 23:52:31 +0500

    Update config.js

--- config.js
+++ config.js
@@ -70,7 +70,7 @@ global.mess = {
     error: 'An error occurd!',
     banned: 'You are *Banned* fron using commands!',
     bangc: 'This Group is *Banned* from using Commands!',
-    nonsfw: 'Dont be a pervert Baka! This is not a NSFW enabled group!'
+    nonsfw: 'Get yourself a girlfriend/boyfriend! This is not a porn enabled group!'
     
 }
 
