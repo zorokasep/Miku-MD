c3314e75b7f1ef155ef1d33a59552a47a6a216ef
zorokasep <128733613+zorokasep@users.noreply.github.com>
2023-03-30 01:33:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.OwnerNumber = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.ownertag = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.Owner = ['6283110038299']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.OwnerNumber = ['6283110038299']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.ownertag = ['6283110038299']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
 global.BotName = "Miku"
 global.packname = "Miku bot"
 global.author = "By: Fantox"
